968de38ba656e146ee05234665d39ae973ca847a
dev-rahim <rahimuddin5915@gmail.com>
2022-09-15 12:20:33 +0600

    working with the sports db api

--- js/the-sports-db.js
+++ js/the-sports-db.js
@@ -1,19 +1,39 @@
 const loadData = async () => {
-    const res = await fetch('https://www.thesportsdb.com/api/v1/json/2/searchplayers.php?p=x');
-    const data = await res.json();
-    getPlayers(data);
+    const searchInput = document.getElementById('searchInput');
+    if (searchInput.value == '') {
+        const btn = document.getElementById('search-btn').setAttribute(alert("Invalid Input"));
+    } else {
+
+        const res = await fetch(`https://www.thesportsdb.com/api/v1/json/2/searchplayers.php?p=${searchInput.value}`);
+        const data = await res.json();
+        searchInput.value = '';
+        console.log(data);
+        if (data.player == null || data.player == []) {
+            document.getElementById('playerDetails').innerHTML = `
+            <h2 class="text-danger text-center">Nothing Found</h2>
+            `;
+            document.getElementById('searchResult').textContent = '';
+
+        } else {
+            document.getElementById('playerDetails').textContent = '';
+            getPlayers(data.player);
+
+        }
+    }
     // fetch('https://www.thesportsdb.com/api/v1/json/2/searchplayers.php?p=x')
     //     .then(res => res.json())
     //     .then(data => console.log(data))
+
 }
-loadData();
+
 const getPlayers = (data) => {
     const searchResult = document.getElementById('searchResult');
-    data.player.forEach(element => {
+    searchResult.textContent = '';
+    data.forEach(element => {
         const div = document.createElement('div');
         div.classList.add('card-group');
         div.innerHTML = `
-                <div class="card m-3">
+                <div onclick="playerDetails('${element.strPlayer}')" class="card m-3">
                     <img class="card-img-top" src="${element.strThumb}" alt="image not found">
                     <div class="card-body">
                         <h5 class="card-title">${element.strPlayer}</h5>
@@ -22,7 +42,24 @@ const getPlayers = (data) => {
                     </div>
                 </div>
         `;
-        searchResult.appendChild(div)
+        searchResult.appendChild(div);
     });
-
+}
+const playerDetails = async (name) => {
+    const res = await fetch(`https://www.thesportsdb.com/api/v1/json/2/searchplayers.php?p=${name}`);
+    const data = await res.json();
+    const element = data.player[0];
+    console.log(data);
+    document.getElementById('playerDetails').innerHTML = `
+                <div onclick="playerDetails('${element.strPlayer}')" class="card m-3">
+                    <img class="card-img-top" src="${element.strThumb}" alt="image not found">
+                    <div class="card-body">
+                        <h5 class="card-title">${element.strPlayer}</h5>
+                        <h6><strong>${element.strNationality}</strong></h6>
+                        <p class="card-text"> Location: ${element.strBirthLocation}</p>
+                        <br>
+                        <p class="card-text">${element.strDescriptionEN}</p>
+                    </div>
+                </div>
+    `;
 }
